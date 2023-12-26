2b5277c6a1efc8f946650d13211250cc379e5db2
prasaddds <durgaprasad.78257@gmail.com>
2023-12-26 05:50:29 +0530

    Your commit message here

--- src/1LoginSignup/Pages/Register.js
+++ src/1LoginSignup/Pages/Register.js
@@ -1,29 +1,64 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
+import axios from 'axios';
+import { toast } from 'react-hot-toast';
+import { useNavigate } from 'react-router-dom';
 
 export default function Register() {
+    const navigate = useNavigate();
     const [data, setData] = useState({
         name: '',
-        password: '',
-        confirmPassword: '',
-        email: ''
-    })
-    const registerUser = (e) => {
-        console.log("Hell");
+        email: '',
+        password: ''
+    });
+
+    const registerUser = async (e) => {
         e.preventDefault();
-    }
+        const { name, email, password } = data;
+
+        try {
+            const data = await axios.post('/register', { name, email, password })
+            if (data.error) {
+                toast.error(data.error);
+            } else {
+                setData({}); // Resetting to initial state
+                toast.success(`Registered successfully`);
+                navigate('/login');
+            }
+        } catch (error) {
+            console.log(error);
+            toast.error('Registration failed. Please try again.'); // Informing the user about the failure
+        }
+    };
+
     return (
         <div>
             <form onSubmit={registerUser}>
                 <label>Enter name</label>
-                <input type='text' placeholder='Enter name...' value={data.name} onChange={(e) => setData({ ...data, name: e.target.value })} /> <br /> <br />
+                <input
+                    type="text"
+                    placeholder="Enter name..."
+                    value={data.name}
+                    onChange={(e) => setData({ ...data, name: e.target.value })}
+                />{' '}
+                <br /> <br />
                 <label>Enter email</label>
-                <input type='email' placeholder='Enter email...' value={data.email} onChange={(e) => setData({ ...data, email: e.target.value })} /> <br /> <br />
+                <input
+                    type="email"
+                    placeholder="Enter email..."
+                    value={data.email}
+                    onChange={(e) => setData({ ...data, email: e.target.value })}
+                />{' '}
+                <br /> <br />
                 <label>Enter password</label>
-                <input type='password' placeholder='Enter password...' value={data.password} onChange={(e) => setData({ ...data, password: e.target.value })} /> <br /> <br />
-                <label>Confirm password</label>
-                <input type='password' placeholder='Confirm password...' value={data.confirmPassword} onChange={(e) => setData({ ...data, confirmPassworde: e.target.value })} /> <br />
-                <button type='submit'>submit</button>
+                <input
+                    type="password"
+                    placeholder="Enter password..."
+                    value={data.password}
+                    onChange={(e) => setData({ ...data, password: e.target.value })}
+                />{' '}
+                <br /> <br />
+                <button type="submit">Submit</button>
             </form>
         </div>
-    )
+    );
 }
--- src/App.js
+++ src/App.js
@@ -3,10 +3,15 @@ import Login from './1LoginSignup/Pages/Login';
 import Register from './1LoginSignup/Pages/Register';
 import Navbar from './1LoginSignup/Components/Navbar';
 import Home from './1LoginSignup/Pages/Home';
+import axios from 'axios';
+import {Toaster} from 'react-hot-toast'
+axios.defaults.baseURL=`http://localhost:8000`
+axios.defaults.withCredentials=true
 function App() {
   return (
     <>
     <Navbar/> <br/>
+    <Toaster position='bottom-right' toastOptions={{duration:2000}}/>
     <Routes>
       <Route path='/' element={<Home/>}/>
       <Route path='/login' element={<Login/>}/>
